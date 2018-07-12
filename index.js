e24516f788f67579ff7e94e6fc9d869867810e97
tash02 <tash.brinkley@gmail.com>
2018-07-12 22:07:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // Code your solution in this file.
-
 function lowerCaseStudentNames () {
-  students = students.map(function(a) { 
-    a.students = a.students.toLowerCase();
-    return a;
-});
+  
 }
-
